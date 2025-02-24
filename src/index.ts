cfc4d691e025abc8bc0cc0ac809949d1d2ecf210
Jan Philipp Michel <j.michel@softfair.de>
2025-02-24 20:46:38 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import { Elysia } from "elysia";
 import { swagger } from '@elysiajs/swagger'
 import { customerRoutes } from "./routes/customerRoutes";
 import { connectDB } from "./db/connection";
+import { cors } from '@elysiajs/cors'
 
 connectDB().then(() => {
   const app = new Elysia()
+    .use(cors())
     .use(swagger({
+      theme: 'light',
       scalarConfig: {
         theme: 'solarized'
       }
@@ -14,7 +17,7 @@ connectDB().then(() => {
       app
         .use(customerRoutes)
     )
-    .listen(process.env.PORT ?? 3002);
+    .listen(process.env.PORT ?? 3000);
 
   console.log(
     `🚀 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
